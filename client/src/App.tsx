6695e3ad9499539cffd3740d8d82d33646f0b212
Shrief Essam <shriefessam1999@gmail.com>
2023-10-06 15:44:56 +0300

    add useeffect to fetch data from queryParms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,18 @@
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParms = new URLSearchParams(queryString);
+    const accessToken = urlParms.get('access_token');
+    const refreshToken = urlParms.get('refresh_token');
+
+    console.log(accessToken);
+    console.log(refreshToken);
+  },[])
+
+
   return (
     <>
       <a href="http://localhost:3000/login">
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express = require('express')
-import dotenv = require('dotenv')
-import * as queryString from 'querystring'
-import randomstring = require('randomstring')
-import { Buffer } from 'buffer'
-import axios, { type AxiosResponse } from 'axios'
+import express = require("express")
+import dotenv = require("dotenv")
+import * as queryString from "querystring"
+import randomstring = require("randomstring")
+import { Buffer } from "buffer"
+import axios from "axios"
 
 dotenv.config()
 const { PORT, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env
@@ -11,61 +11,70 @@ const { PORT, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env
 const app = express()
 
 const port = parseInt(PORT as string)
-const host = 'localhost'
+const host = "localhost"
 
-app.get('/', (req, res) => {
-  res.send('testing....')
+app.get("/", (req, res) => {
+  res.send("testing....")
 })
 
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   const state = randomstring.generate(16)
-  const scope = 'user-read-private user-read-email'
-  const stateKey = 'spotify_auth_state'
+  const scope = "user-read-private user-read-email"
+  const stateKey = "spotify_auth_state"
 
   res.cookie(stateKey, state)
 
-  res.redirect('http://accounts.spotify.com/authorize?' +
-    queryString.stringify({
-      response_type: 'code',
-      client_id: CLIENT_ID,
-      scope,
-      redirect_uri: REDIRECT_URI,
-      state
-    })
+  res.redirect(
+    "http://accounts.spotify.com/authorize?" +
+      queryString.stringify({
+        response_type: "code",
+        client_id: CLIENT_ID,
+        scope,
+        redirect_uri: REDIRECT_URI,
+        state
+      })
   )
 })
 
-app.get('/callback', async (req, res) => {
-  const code = req.query.code as string ?? null
+app.get("/callback", async (req, res) => {
+  const code = (req.query.code as string) ?? null
   const state = req.query.state ?? null
 
   if (state == null) {
-    res.redirect('/#' + queryString.stringify({ error: 'state mismatch' }))
+    res.redirect("/#" + queryString.stringify({ error: "state mismatch" }))
   } else {
     try {
       const response = await axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
+        method: "post",
+        url: "https://accounts.spotify.com/api/token",
         data: queryString.stringify({
-          grant_type: 'authorization_code',
+          grant_type: "authorization_code",
           code,
           redirect_uri: REDIRECT_URI
         }),
         headers: {
-          'Content-type': 'application/x-www-form-urlencoded',
-          Authorization: 'Basic ' + (Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+          "Content-type": 'application/x-www-form-urlencoded',
+          Authorization:
+            "Basic " +
+            Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64")
         }
       })
 
       if (response.status === 200) {
-        try {
-          const userData = await getUserData(response)
-          res.send(userData)
-        } catch (err) {
-          console.log(err)
-        }
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        const { access_token, refresh_token } = response.data
+        const queryParms = queryString.stringify({
+          access_token,
+          refresh_token
+        })
+
+        res.redirect(`http://localhost:5173/?${queryParms}`)
       } else {
-        res.send(response)
+        res.redirect(
+          `/?${queryString.stringify({
+            error: "invalid token"
+          })}`
+        )
       }
     } catch (err) {
       console.log(err)
@@ -102,19 +111,3 @@ app.get('/callback', async (req, res) => {
 app.listen(port, host, () => {
   console.log(`Express app is listening on: http://${host}:${port}`)
 })
-
-const getUserData = async (response: AxiosResponse): Promise<string | AxiosResponse<any, any>> => {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  const { access_token, token_type } = response.data
-  const userResponse = await axios.get('http://api.spotify.com/v1/me', {
-    headers: {
-      Authorization: `${token_type} ${access_token}`
-    }
-  })
-
-  if (userResponse.status === 200) {
-    return `<pre>${JSON.stringify(userResponse.data, null, 2)}</pre>`
-  } else {
-    return response
-  }
-}
