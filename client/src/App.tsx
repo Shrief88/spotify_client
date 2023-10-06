9dccd0ebafed886cca76fa8b067149a956339861
Shrief Essam <shriefessam1999@gmail.com>
2023-10-06 15:13:54 +0300

    create react app using vite

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <a href="http://localhost:3000/login">
+        <button>login</button>
+      </a>
+    </>
+  );
+}
+
+export default App;
